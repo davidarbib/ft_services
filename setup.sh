44e849f0ebe5a40c0ed157edf38fad371667faea
user42 <davidarbib@yahoo.fr>
2020-09-10 18:26:24 +0200

    nginx with nodeport access works

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 #sudo usermod -aG docker user42; newgrp docker
 #echo "test"
-sudo chown -R user42:user42 ~/.minikube
+#sudo chown -R user42:user42 ~/.minikube
 minikube --vm-driver=docker start
 
 # Nginx
+eval $(minikube docker-env)
 docker build -t nginx_custom ./nginx_srcs
 minikube cache add nginx_custom:latest

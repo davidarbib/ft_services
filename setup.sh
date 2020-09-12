f412dae0ecdc6218b8f27a415541bca5b8cb9b76
user42 <davidarbib@yahoo.fr>
2020-09-12 18:59:46 +0200

    nginx ssl in progress

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+
 #sudo usermod -aG docker user42; newgrp docker
 #echo "test"
 #sudo chown -R user42:user42 ~/.minikube
+
 minikube --vm-driver=docker start
 
-# Nginx
 eval $(minikube docker-env)
-docker build -t nginx_custom ./nginx_srcs
-minikube cache add nginx_custom:latest
+
+# Nginx
+docker build -t nginx_custom:v1.01 ./nginx_srcs
+kubectl apply -f nginx_srcs/deployment.yaml
+kubectl apply -f nginx_srcs/service.yaml

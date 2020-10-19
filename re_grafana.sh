2fa80e4b1fe3ebdc7d241565d9bae8d3bfa98915
user42 <davidarbib@yahoo.fr>
2020-10-19 14:44:44 +0200

    grafana launched

--- re_grafana.sh
+++ re_grafana.sh
@@ -2,5 +2,5 @@
 docker stop grafana
 docker rm grafana
 docker build -t grafana:v1 grafana/.
-docker run -ti --name grafana -p 8090:80 grafana:v1
+docker run -ti --name grafana -p 3000:3000 grafana:v1
 #docker run -ti --name sql_box -p 8080:80 mysql_custom:v1

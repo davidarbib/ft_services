2c2aa60368480b8b347ab9ef5614319f8c69bed4
user42 <davidarbib@yahoo.fr>
2020-11-03 16:02:20 +0100

    update minikube and kubernetes, changes for metallb and services IP

--- setup.sh
+++ setup.sh
@@ -1,42 +1,46 @@
 #!/bin/sh
 
-#sudo usermod -aG docker user42; newgrp docker
-#echo "test"
-#sudo chown -R user42:user42 ~/.minikube
+##---download and install---
+#curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+#sudo install minikube-linux-amd64 /usr/local/bin/minikube
+##---------------------------
 
-minikube start --vm-driver=docker
+minikube start --vm-driver=docker --kubernetes-version=v1.19.2
 
+#extract docker infos from minikube env to invoke minikube docker from here
 eval $(minikube docker-env)
 
 #kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin \
 #--serviceaccount=kube-system:default
 
 # MetalLB
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+#kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+minikube addons enable metallb
+
 kubectl apply -f metallb/configmap.yaml
 
-# Nginx
-docker build -t nginx_custom:v1.01 ./nginx_srcs
-kubectl apply -f nginx_srcs/deployment.yaml
-kubectl apply -f nginx_srcs/service.yaml
+## Nginx
+#docker build -t nginx_custom:v1.01 ./nginx_srcs
+#kubectl apply -f nginx_srcs/deployment.yaml
+#kubectl apply -f nginx_srcs/service.yaml
 #
 ## FTPS
 #docker build -t ftps_custom:v1 ./ftps
 #kubectl apply -f ftps/deployment.yaml
 #kubectl apply -f ftps/service.yaml
 #
-# MySQL
-docker build -t mysql_custom:v1 ./mysql
-kubectl apply -f mysql/deployment.yaml
-kubectl apply -f mysql/service.yaml
+## MySQL
+#docker build -t mysql_custom:v1 ./mysql
+#kubectl apply -f mysql/deployment.yaml
+#kubectl apply -f mysql/service.yaml
 #
-# Wordpress
-docker build -t wordpress_custom:v1 ./wordpress
-kubectl apply -f wordpress/deployment.yaml
-kubectl apply -f wordpress/service.yaml
-#
+## Wordpress
+#docker build -t wordpress_custom:v1 ./wordpress
+#kubectl apply -f wordpress/deployment.yaml
+#kubectl apply -f wordpress/service.yaml
+##
 ## PhpMyAdmin
 #docker build -t phpmyadmin_custom:v1 ./phpmyadmin
 #kubectl apply -f phpmyadmin/deployment.yaml
